a58a5c5b385c90dc80e59c69ff08e6ae516c28d0
Hikl845 <freelancer198756@gmail.com>
2025-10-17 01:16:07 +0300

    Update MyLinkedList.java

--- src/main/java/org/example/MyLinkedList.java
+++ src/main/java/org/example/MyLinkedList.java
@@ -1,78 +1,86 @@
 package org.example;
 
-public class MyLinkedList {
+public class MyLinkedList<T> {
 
-    public static class Node{
-    Object value;
-    Node next;
-    Node prev;
-    Node(Object value){
-        this.value = value;
-    }
-}
-private Node head;
-private Node tail;
-private int size = 0;
-public void add(Object value){
-    Node newNode = new Node(value);
-    if(head == null){
-        head = tail = newNode;
-    }
-    else{
-        newNode.prev = tail;
-        tail.next = newNode;
-        tail = newNode;
-    }
-    size++;
-}
-    private void checkIndex(int index) {
-        if (index < 0 || index >= setSize) {
-            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + setSize);
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+        Node<T> prev;
+
+        Node(T value) {
+            this.value = value;
         }
     }
-public String get(int index){
-checkIndex(index);
-return getNode(index).value.toString();
-}
-public void remove(int index){
-    checkIndex(index);
-    Node toRemove = getNode(index);
 
-    if (toRemove.prev != null) {
-        toRemove.prev.next = toRemove.next;
-    } else {
-        head = toRemove.next;
+    private Node<T> head;
+    private Node<T> tail;
+    private int size = 0;
+
+    public void add(T value) {
+        Node<T> newNode = new Node<>(value);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.prev = tail;
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
     }
 
-    if (toRemove.next != null) {
-        toRemove.next.prev = toRemove.prev;
-    } else {
-        tail = toRemove.prev;
+    public T get(int index) {
+        checkIndex(index);
+        return getNode(index).value;
     }
 
-    size--;
-}
-    public int size(){
+    public void remove(int index) {
+        checkIndex(index);
+        Node<T> toRemove = getNode(index);
+
+        if (toRemove.prev != null) {
+            toRemove.prev.next = toRemove.next;
+        } else {
+            head = toRemove.next;
+        }
+
+        if (toRemove.next != null) {
+            toRemove.next.prev = toRemove.prev;
+        } else {
+            tail = toRemove.prev;
+        }
+
+        size--;
+    }
+
+    public int size() {
         return size;
     }
-    public void clear(){
-    head = null;
-    tail = null;
-    size = 0;
+
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
     }
-    private Node getNode(int index) {
-        Node current;
-        if (index < setSize / 2) {
+
+    private Node<T> getNode(int index) {
+        Node<T> current;
+        if (index < size / 2) {
             current = head;
             for (int i = 0; i < index; i++) {
                 current = current.next;
             }
         } else {
             current = tail;
-            for (int i = setSize - 1; i > index; i--) {
+            for (int i = size - 1; i > index; i--) {
                 current = current.prev;
             }
         }
         return current;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
     }
 }
